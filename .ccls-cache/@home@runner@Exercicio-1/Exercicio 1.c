8fe950abcba4c28cbe1113ec53f92543b6c47ef8
lipefrances <20511456-lipefrances@users.noreply.replit.com>
2023-04-18 00:00:14 +0000

    exercicios_alg_1_C

---
+++ .ccls-cache/@home@runner@Exercicio-1/Exercicio 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &num);
+
+    printf("O numero digitado foi: %d", num);
+
+    return 0;
+}
